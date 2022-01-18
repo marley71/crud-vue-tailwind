d99656fa9d6e0a17d8e2ec5f2fc91a9b74308afc
vagrant <vagrant@homestead>
2022-01-18 17:02:25 +0000

    miglioramenti

--- src/components/app/cManage.vue
+++ src/components/app/cManage.vue
@@ -11,7 +11,7 @@
             <div c-edit-area>
                 <div class="portlet-header border-bottom mb-3" :class="$crud.env.layoutGradientColor">
                         <span class="d-block text-white text-truncate font-weight-medium" v-show="updateTitle">
-                          {{updateTitle}}
+                          {{ updateTitle }}
                         </span>
                 </div>
                 <div class="portlet-body pb-0" c-edit-container>
@@ -20,12 +20,12 @@
             </div>
         </div>
         <span c-view_dialog class="modal hidden" tabindex="-1" role="dialog"
-             aria-labelledby="" aria-hidden="true"
+              aria-labelledby="" aria-hidden="true"
         >
-            <div class="w-3/4"  role="document">
+            <div class="w-3/4" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">{{viewTitle}}</h5>
+                        <h5 class="modal-title">{{ viewTitle }}</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span class="fi fi-close fs--18" aria-hidden="true"></span>
                         </button>
@@ -52,39 +52,50 @@
 <script>
 
 import cComponent from '../misc/cComponent'
-import { crud, cManageMixin } from 'crud-vue-core'
+import {crud, cManageMixin} from 'crud-vue-core'
 
 crud.conf['c-manage'].collapsible = false
 crud.conf['c-manage'].collapsed = false
 crud.conf['c-manage'].collapseId = null
 
-export default {
-  name: 'c-manage',
-  extends: cComponent,
-  // props: ['cModel', 'cInlineEdit', 'cCollapsible'],
-  mixins: [cManageMixin],
-  methods: {
-    _dynamicData (conf) {
-      conf.collapseId = 'manageCollapse' + conf.modelName
-      return conf
-    },
-      showInsert() {
-          this.jQe('[c-edit-area]').removeClass('hidden');
-          this.jQe('[c-list-container]').addClass('hidden');
-          this.jQe('[c-search-container]').addClass('hidden');
-      },
-    showEdit() {
-        this.jQe('[c-edit-area]').removeClass('hidden');
-        this.jQe('[c-list-container]').addClass('hidden');
-        this.jQe('[c-search-container]').addClass('hidden');
-    },
-      showList() {
-          this.jQe('[c-edit-area]').addClass('hidden');
-          this.jQe('[c-list-container]').removeClass('hidden');
-          this.jQe('[c-search-container]').removeClass('hidden');
-      }
+crud.conf['c-manage'].listSelector = '[c-list-container]';
+crud.conf['c-manage'].searchSelector = '[c-search-container]';
+crud.conf['c-manage'].updateAreaSelector = '[c-edit-area]';
+crud.conf['c-manage'].updateSelector = '[c-edit-container]';
+crud.conf['c-manage'].viewSelector = '[c-view-container]';
 
-  }
+export default {
+    name: 'c-manage',
+    extends: cComponent,
+    // props: ['cModel', 'cInlineEdit', 'cCollapsible'],
+    mixins: [cManageMixin],
+    methods: {
+        _dynamicData(conf) {
+            conf.collapseId = 'manageCollapse' + conf.modelName
+            return conf
+        },
+        showInsert() {
+            this.jQe(this.updateAreaSelector).removeClass('hidden');
+            this.jQe(this.listSelector).addClass('hidden');
+            this.jQe(this.searchSelector).addClass('hidden');
+        },
+        showEdit() {
+            this.jQe(this.updateAreaSelector).removeClass('hidden');
+            this.jQe(this.listSelector).addClass('hidden');
+            this.jQe(this.searchSelector).addClass('hidden');
+        },
+        showList() {
+            var that = this;
+            if (!that.listComp) {
+                console.log('cManage.showList, listComp non valido');
+                return;
+            }
+            this.jQe(this.updateAreaSelector).addClass('hidden');
+            this.jQe(this.listSelector).removeClass('hidden');
+            this.jQe(this.searchSelector).removeClass('hidden');
+        }
+
+    }
 }
 </script>
 
--- src/components/widgets/wRadio.vue
+++ src/components/widgets/wRadio.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="col-12 mb-2 form-label-group form-control bw--1 border-solid border-gray-400 p--6 rounded">
-        <div class="in-label-group">
-            <label v-for="key in domainValuesOrder" class="" :key="key">
-                <input class="form-radio" :name="getFieldName()" type="radio" :value="key"
-                       v-model="value"
-                       v-on:change="change">
-                <i></i><span v-html="domainValues[key]"></span>
-            </label>
+    <div class="w-100 flex flex-row items-center mr-4 mb-4" :class="inline?'flex-row':'flex-col'">
+        <div v-for="key in domainValuesOrder" :key="key" >
+            <input type="radio" :name="getFieldName()" class="hidden" v-model="value" v-on:change="change" />
+            <label for="radio1" class="flex items-center cursor-pointer text-xl">
+                <span class="w-8 h-8 inline-block mr-2 rounded-full border border-grey flex-no-shrink"></span>
+                {{ domainValues[key] }}</label>
         </div>
     </div>
+
+
+<!--    <div class="col-12 mb-2 form-label-group form-control bw&#45;&#45;1 border-solid border-gray-400 p&#45;&#45;6 rounded">-->
+<!--        <div class="in-label-group">-->
+<!--            <label v-for="key in domainValuesOrder" class="" :key="key">-->
+<!--                <input class="form-radio" :name="getFieldName()" type="radio" :value="key"-->
+<!--                       v-model="value"-->
+<!--                       v-on:change="change">-->
+<!--                <i></i><span v-html="domainValues[key]"></span>-->
+<!--            </label>-->
+<!--        </div>-->
+<!--    </div>-->
 </template>
 
 <script>
@@ -23,5 +33,23 @@ export default {
 </script>
 
 <style scoped>
+input[type="radio"] + label span {
+    transition: background .2s,
+    transform .2s;
+}
+
+input[type="radio"] + label span:hover,
+input[type="radio"] + label:hover span{
+    transform: scale(1.2);
+}
+
+input[type="radio"]:checked + label span {
+    background-color: #3490DC;
+box-shadow: 0px 0px 0px 2px white inset;
+}
+
+input[type="radio"]:checked + label{
+    color: #3490DC;
+}
 
 </style>
