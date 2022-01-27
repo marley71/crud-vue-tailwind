466daa09a4bea66bdce6e8564101010a96e10c76
vagrant <vagrant@homestead>
2022-01-27 13:47:15 +0000

    miglioramenti

--- src/components/actions/aGrouped.vue
+++ src/components/actions/aGrouped.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="flex flex-col" v-show="_visible" :title="title | translate" :disabled="_disabled" >
-        <button type="button" href="#" :class="css" v-on:click="toggle()" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true">
-              <i class="fa fa-bars" :class="!open?'':'hidden'"></i>
-              <i class="fa-solid fa-square-xmark" :class="open?'':'hidden'"></i>
+        <button type="button" :class="css" v-on:click="toggle()">
+<!--              <i class="fa fa-bars" :class="!open?'':'hidden'"></i>-->
+<!--              <i class="fa-solid fa-square-xmark" :class="open?'':'hidden'"></i>-->
+            <i class="fa fa-bars" v-show="!opened"></i>
+            <i class="fa-solid fa-square-xmark" v-show="opened"></i>
         </button>
-        <div class="flex flex-col z-40 absolute mt-10 ml-2 bg-brand-50 rounded p-1" :class="open?'':'hidden'">
+        <div class="flex flex-col z-40 absolute mt-10 ml-2 bg-brand-50 rounded p-1" :class="opened?'':'hidden'">
             <template v-for="(action,name) in actionsConfig">
                 <component :is="action.componentName" :c-conf="action" inline-template :key="name">
                     <a class="dropdown-item text-truncate cursor-pointer" v-if="_visible"
@@ -22,14 +24,14 @@
 import cComponent from '../misc/cComponent'
 import {aGroupedMixin,crud} from 'crud-vue-core'
 
-crud.conf['a-grouped'].open = false;
+crud.conf['a-grouped'].opened = false;
 export default {
   name: 'a-grouped',
   extends: cComponent,
   mixins: [aGroupedMixin],
     methods: {
       toggle() {
-          this.open = !this.open;
+          this.opened = !this.opened;
       }
     }
 }
